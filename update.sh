ecdd79cba5ad533760063032ab8e94c5829b9c2b
Clint Egargo <clintegargo@protonmail.ch>
2023-06-06 01:36:01 +0800

    Update: Tue  6 Jun 01:36:01 PST 2023

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 cp -v  ~/.config/alacritty/alacritty.yml .config/alacritty/ 2>/dev/null
 cp -v  ~/.config/Code/User/settings.json .config/Code/User/ 2>/dev/null
-cp -v  ~/.config/nvim/init.vim .config/nvim/ 2>/dev/null
+cp -v  ~/.config/nvim/* .config/nvim/ 2>/dev/null
 cp -v  ~/.config/starship.toml .config/ 2>/dev/null
 cp -vR ~/.fonts/*.ttf .fonts/ 2>/dev/null
 cp -v  ~/.tmux.conf . 2>/dev/null
