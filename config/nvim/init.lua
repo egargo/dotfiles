eb32fc7b720cbec4b81c55aa1a9300c47330a3fc
Clint Egargo <clintegargo@protonmail.ch>
2023-06-12 14:19:11 +0800

    Update: Mon 12 Jun 14:19:11 PST 2023

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -113,8 +113,12 @@ require('lazy').setup({
     },
   },
 
-  -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',          opts = {} },
+  {
+    -- Useful plugin to show you pending keybinds.
+    'folke/which-key.nvim',
+    opts = {}
+  },
+
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
