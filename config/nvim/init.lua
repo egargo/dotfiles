4cce907e1af0815abe3b66de0fd5fd25c7b08c6c
Clint Egargo <clintegargo@protonmail.ch>
2023-06-23 23:17:51 +0800

    Update: Fri 23 Jun 23:17:51 PST 2023

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -38,8 +38,8 @@ P.S. You can delete this when you're done too. It's your config now :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
+vim.g.mapleader = ';'
+vim.g.maplocalleader = ';'
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
