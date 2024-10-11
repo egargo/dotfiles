222111995f3120226ad239ec81b649fceb04ea15
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-10-11 14:29:50 +0800

    update neovim configuration

---
+++ config/nvim/lua/bee/plugins/toggleterm.lua
@@ -0,0 +1,20 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			size = 12,
+			desc = "Open [t]erminal Open",
+			open_mapping = "<C-t>",
+			hide_numbers = true,
+			direction = "horizontal", -- "float" | "horizontal" | "vertical"
+			close_on_exit = true,
+			shell = vim.o.shell,
+			float_opts = {
+				border = "single",
+				width = 132,
+				height = 32,
+			},
+		})
+	end,
+}
---
+++ config/nvim/lua/bee/vim.lua
@@ -0,0 +1,78 @@
+-- vim.g.*
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
+vim.g.editorconfig = true
+
+-- vim.o.*
+vim.o.splitright = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.smartindent = true
+vim.o.relativenumber = false
+vim.o.cursorline = true
+vim.o.ruler = true
+
+-- vim.opt.*
+vim.opt.number = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = {
+	tab = "» ",
+	trail = "·",
+	nbsp = "␣",
+	-- eol = "↴"
+}
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.hlsearch = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.list = true
+
+-- vim.wo.*
+vim.wo.number = true
+
+-- vim.keymap.*
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!!"<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "[G]oto [D]efinition" })
+vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "[G]oto [D]eclaration" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
+vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- vim.api.*
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
