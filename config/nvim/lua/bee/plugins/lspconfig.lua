49b37f75b5ce816ebdd91adfa7252d896a19fd79
Clint Egargo <67180945+egargo@users.noreply.github.com>
2025-02-23 13:23:27 +0800

    Update: Sun Feb 23 01:23:27 PM PST 2025

--- config/nvim/lua/bee/plugins/lspconfig.lua
+++ config/nvim/lua/bee/plugins/lspconfig.lua
@@ -176,7 +176,7 @@ return {
 		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
-			"stylua", -- Used to format Lua code
+			-- "stylua", -- Used to format Lua code
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
